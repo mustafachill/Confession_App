e08607399cabb953748619d066baeef160466570
Mustafa Çil <mustafacilbusiness@gmail.com>
2024-05-31 14:49:02 +0300

    Project settings completed

--- confession_app_project/confession_app/views.py
+++ confession_app_project/confession_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def listconfesion(request):
+    return render(request,'confession_app/listconfession.html')
+
+def addconfesion(request):
+    return render(request,'confession_app/addconfession.html')
